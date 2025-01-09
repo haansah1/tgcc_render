9a40d161600f27c5b3afabedbe6abff1203451c7
oncampus.teamgh@gmail.com <oncampus.teamgh@gmail.com>
2025-01-09 09:18:06 +0000

    Deploying to render

--- static/main1.js
+++ static/main1.js
@@ -2,7 +2,8 @@ var hidEl = document.querySelector(".hid")
   var barsEl = document.querySelector(".divbars")
   var inputEl = document.getElementById("input")
   var closeEl = document.querySelector(".close")
-  var windowEl = document.getElementBy
+  // var windowEl = document.getElementBy
+  var welcomeEl = document.querySelector(".welcome-wrapper")
 
   inputEl.addEventListener("input", ()=>{
     if(inputEl.checked){
@@ -25,45 +26,24 @@ var hidEl = document.querySelector(".hid")
     hidEl.style.transform = "scaleY(0)";
   })
 
+  const sensitivity = 70;
 
-  welcomeEl = document.querySelectorAll('.welc-p')
-  headerEl = document.querySelector('[class^=header]')
-  
-  console.log(welcomeEl1.offsetTop)
-  console.log(headerEl.offsetHeight)
-  
-  let animate = false;
-  window.addEventListener("scroll", ()=> {
-      console.log("l,fldkl")
-      if(window.scrollY > headerEl.offsetTop - welcomeEl[1].offsetHeight+600 && !animate){
-          gsap.from(welcomeEl, {
-            opacity: 1,
-            y: 200,
-            duration: 1,
-            stagger: 0.2,
-            yoyo: true,
-            ease: 'power3.out',
-            delay: .5,
-          })
-        // }
-        animate = true
-        console.log("k");
-        }
-        else if (window.scrollY < headerEl.offsetTop - welcomeEl[1].offsetHeight+600 && animate){
-          console.log("sldk");
-            gsap.from(welcomeEl, {
-              opacity:0 ,
-              y: 0,
-              stagger: 0.2,
-              yoyo: true,
-              // ease: 'power3.out',
-            })
-          // }
-          animate = false;
-        }
-    }
+  welcomeEl.addEventListener("mousemove", (e)=>{
+
     
-)
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+
+    const mouseX = e.clientX / windowWidth;
+    const mouseY = e.clientY / windowHeight;
+
+    const moveX = (mouseX - 0.5) * sensitivity;
+    const moveY = (mouseY - 0.5) * sensitivity;
+
+    welcomeEl.style.backgroundPosition = `${50 - moveX}%, ${50 - moveY}%`;
+
+  });
+
   
 
 
